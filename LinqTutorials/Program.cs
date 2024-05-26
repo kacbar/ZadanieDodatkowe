c7fd88b56988aa2ef358e3e16fe8f294797723e4
s25101 <s25101@pjwstk.edu.pl>
2024-05-26 23:18:48 +0200

    Initial Project

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task1();
+            var a = LinqTasks.Task2();
+            var b = LinqTasks.Task3();
+            var c = LinqTasks.Task4();
+            var dEnumerable = LinqTasks.Task5();
+            var et = LinqTasks.Task6();
+            var ft = LinqTasks.Task7();
+            var gt = LinqTasks.Task8();
+            var at = LinqTasks.Task9();
+            var ca = LinqTasks.Task10();
+            var db = LinqTasks.Task11();
+            var fc = LinqTasks.Task12();
+            //var hdEnumerable = LinqTasks.Task13({});
+            var get = LinqTasks.Task14();
+            //var fft = LinqTasks.Task15();
+
+
+        }
+    }
+}
